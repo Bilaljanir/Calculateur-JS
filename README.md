2a1385df1d3c9e80ad4060bba2bee7a94699a6c9
bilaljanir <bilaljanir@gmail.com>
2022-12-12 09:15:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mon calculateur JS
---
+++ index.js
@@ -0,0 +1,39 @@
+var tableau1= [];
+var tableau2= [];
+var tableau3= [];
+var tableau4= [];
+var tableau5= [];
+
+
+
+let allButton = document.getElementsByClassName("addButton")
+for (const button of allButton) {
+    button.addEventListener("click", (e) => {
+        let id = e.target.id.split("-")
+        id[0] = 'grade'
+        let inputGrade = parseFloat(document.getElementById(id.join("-")).value)
+        id[0] = 'description'
+        let inputDesc = document.getElementById(id.join("-")).value
+
+        console.log(inputDesc);
+        console.log(inputGrade);
+        console.log(id);
+      
+    });
+}
+
+
+// let inputGrade = document.getElementById("grade-ecole_pro-semestre1")
+// let inputDesc = document.getElementById("description-ecole_pro-semestre1")
+
+
+        // savoir ou vous êtes (event target)
+
+        // Remonter dans les éléments html (pour arriver au tableau) html parent
+
+        // Ajouter des lignes dans le tableau (html table)
+
+
+
+
+
